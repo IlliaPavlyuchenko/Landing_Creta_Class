f0fb3b49bae6bbef2c288cf774640a0f2196d259
IlliaPavlyuchenko <ilya.pavlyuchenko@gmail.com>
2025-05-15 02:37:06 +0300

    feat: add swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 const body = document.querySelector('body');
+
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
