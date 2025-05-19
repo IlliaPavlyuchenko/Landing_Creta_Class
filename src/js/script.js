b3f4b63c9bbea5e1ee62d3b3609b3e71147727da
IlliaPavlyuchenko <ilya.pavlyuchenko@gmail.com>
2025-05-19 03:35:08 +0300

    feat: add skills_tab block

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,35 @@ const swiper = new Swiper('.swiper', {
 		clickable: true,
 	},
 });
+
+function tabs(tabsBlockSelector) {
+	const tabsNavAll = document.querySelectorAll(`${tabsBlockSelector} [data-tab]`);
+	const tabsContentAll = document.querySelectorAll(`${tabsBlockSelector} [data-tab-content]`);
+
+	tabsNavAll.forEach(tabNav => {
+		tabNav.addEventListener('click', event => {
+			// Check if the tab is already active
+			if (tabNav.classList.contains('active')) return;
+			// Remove the active class from all tabs and content
+
+			tabsNavAll.forEach(nav => nav.classList.remove('active'));
+			tabsContentAll.forEach(content => content.classList.remove('active'));
+
+			// Add the active class to the current tab
+			tabNav.classList.add('active');
+
+			// Find the corresponding content
+			const targetContentId = event.currentTarget.dataset.tab;
+			const targetContent = document.querySelector(`#${targetContentId}`);
+
+			// Check if the content exists
+			if (targetContent) {
+				targetContent.classList.add('active');
+			} else {
+				console.error(`Content with id="${targetContentId}" not found!`);
+			}
+		});
+	});
+}
+
+tabs('.tabsBlock1');
