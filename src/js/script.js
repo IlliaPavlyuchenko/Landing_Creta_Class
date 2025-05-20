eabde80f0092b5e3abe20a8ba47b52ffca136fd8
IlliaPavlyuchenko <ilya.pavlyuchenko@gmail.com>
2025-05-20 19:44:52 +0300

    fix: slider

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ const swiper = new Swiper('.swiper', {
 	slidesPerView: 3,
 	loop: true,
 	spaceBetween: 40,
+	slidesPerGroup: 3,
 	// Navigation arrows
 	navigation: {
 		nextEl: '.learningSliderNext',
